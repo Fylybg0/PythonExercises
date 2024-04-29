1609ea431b068d9c7954898483288ca14fd905b0
Fylybg0 <bodorfilip8@gmail.com>
2024-04-29 15:38:28 +0200

    updating the getAverage.py for using it in command prompt

--- getAverage.py
+++ getAverage.py
@@ -1,4 +1,32 @@
+import sys
+from readFile import readFile
+
 def getAverage(array):
     if not array:
         return None
     return sum(array) / len(array)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python getAverage.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
+    try:
+        with open(file_path, 'r') as file:
+            numbers = readFile(file)
+
+    except FileNotFoundError:
+        print("File not found:", file_path)
+        sys.exit(1)
+
+    except ValueError:
+        print("Invalid data found in the file.")
+        sys.exit(1)
+
+    average = getAverage(numbers)
+    print("Average:", average)
+
+if __name__ == "__main__":
+    main()
