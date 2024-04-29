cb52981f747b057bd3a28e69ccc3f143469e022e
Fylybg0 <bodorfilip8@gmail.com>
2024-04-29 15:34:45 +0200

    int -> float

--- readFile.py
+++ readFile.py
@@ -1,5 +1,5 @@
 def readFile(filename):
-    return [int(i) for i in open(filename, 'r').read().strip().split('\n')]
+    return [float(i) for i in open(filename, 'r').read().strip().split('\n')]
 
 numbers = readFile('numbers.txt')
 
